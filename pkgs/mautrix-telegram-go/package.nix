022b5f12c5d46148ac8d6b37aa653bc4dcfcb5d5
Bart Oostveen <bart@bartoostveen.nl>
2026-07-16 14:26:26 +0200

    mautrix-telegram-go: 0.2606.0 -> 0.2607.0
    Diff: https://github.com/mautrix/telegram/compare/v0.2606.0...v0.2607.0
    
    Changelog: https://github.com/mautrix/telegram/blob/refs/tags/v0.2607.0/CHANGELOG.md

--- pkgs/mautrix-telegram-go/package.nix
+++ pkgs/mautrix-telegram-go/package.nix
@@ -9,16 +9,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "mautrix-telegram";
-  version = "0.2606.0";
+  version = "0.2607.0";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "telegram";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-tKoqtGCkUtCT/SMxRX6LzivGu0p/AM6TPDQoW9plTyE=";
+    hash = "sha256-MpdsWtEsVnC6purF5sw+RD+Nb/3Wo0xrzSn2BuFZmj8=";
   };
 
-  vendorHash = "sha256-+VDdJg5RZzMrphJ5SK+YbdENhPiHJpwGY/JqBJewtUo=";
+  vendorHash = "sha256-bmpTm1/6Z+kAFGAJ70ohBz8+n8JZk7mZyCfX0+FB/fE=";
 
   ldflags = [
     "-X"
