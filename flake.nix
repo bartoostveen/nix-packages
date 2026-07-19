a5031c45e1ad83341b648bef9b5aa316d338b342
Bart Oostveen <bart@bartoostveen.nl>
2026-07-20 01:06:56 +0200

    hydraJobs: filter out unnecessary jobs

--- flake.nix
+++ flake.nix
@@ -12,62 +12,82 @@
 
   outputs =
     inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
 
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
+      let
+        inherit (lib)
+          filterAttrs
+          mapAttrs
+          isDerivation
+          ;
+      in
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ];
 
-      flake.overlays = {
-        default =
-          _: prev:
-          import ./default.nix {
-            system = prev.stdenv.system;
-            pkgs = prev;
-          };
-        suppressSystemWarning = _: _: { _bartPackages.suppressSystemWarning = true; };
-      };
+        imports = [
+          inputs.treefmt-nix.flakeModule
+        ];
 
-      flake = {
-        hydraJobs = inputs.self.legacyPackages.x86_64-linux;
-      };
-
-      perSystem =
-        {
-          pkgs,
-          system,
-          lib,
-          ...
-        }:
-
-        let
-          inherit (lib) filterAttrs isDerivation;
-
-          packages = pkgs.callPackage ./default.nix { };
-        in
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
-              permittedInsecurePackages = [
-                "olm-3.2.16"
-              ];
+        flake.overlays = {
+          default =
+            _: prev:
+            import ./default.nix {
+              system = prev.stdenv.system;
+              pkgs = prev;
             };
-          };
-
-          treefmt = {
-            programs.nixfmt.enable = true;
-            programs.deadnix.enable = true;
-          };
-
-          packages = filterAttrs (_: isDerivation) packages;
-          legacyPackages = packages;
+          suppressSystemWarning = _: _: { _bartPackages.suppressSystemWarning = true; };
         };
-    };
+
+        flake = {
+          hydraJobs =
+            inputs.self.legacyPackages
+            |> filterAttrs (system: _: system == "x86_64-linux")
+            |> mapAttrs (
+              _: jobs:
+              mapAttrs (
+                _: job: if !(isDerivation job) && builtins.isAttrs job then filterAttrs (_: isDerivation) else job
+              ) jobs
+              |> filterAttrs (_: job: builtins.isAttrs job || isDerivation job)
+            );
+        };
+
+        perSystem =
+          {
+            pkgs,
+            system,
+            lib,
+            ...
+          }:
+
+          let
+            inherit (lib) filterAttrs isDerivation;
+
+            packages = pkgs.callPackage ./default.nix { };
+          in
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+                permittedInsecurePackages = [
+                  "olm-3.2.16"
+                ];
+              };
+            };
+
+            treefmt = {
+              programs.nixfmt.enable = true;
+              programs.deadnix.enable = true;
+            };
+
+            packages = filterAttrs (_: isDerivation) packages;
+            legacyPackages = packages;
+          };
+      }
+    );
 }
