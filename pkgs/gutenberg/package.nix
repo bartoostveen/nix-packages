a3cad47a4d8223170add99596ac86e18df0aff12
Bart Oostveen <bart@bartoostveen.nl>
2026-07-22 14:01:36 +0200

    gutenberg: 23.5.3 -> 23.6.0
    Diff: https://github.com/WordPress/gutenberg/compare/v23.5.3...v23.6.0
    
    Changelog: https://github.com/WordPress/gutenberg/blob/refs/tags/v23.6.0/changelog.txt

--- pkgs/gutenberg/package.nix
+++ pkgs/gutenberg/package.nix
@@ -9,7 +9,7 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "gutenberg";
-  version = "23.5.3";
+  version = "23.6.0";
 
   __structuredAttrs = true;
   strictDeps = true;
@@ -18,7 +18,7 @@ buildNpmPackage (finalAttrs: {
     owner = "WordPress";
     repo = "gutenberg";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VTK1JxFFabqi2rkBFqh7HJ09e2aJ2RfnfHu5RKe4wCc=";
+    hash = "sha256-gfCkBFaWJIa2tGPmCjmLhErMCH88O6CgNyrODZtZpfo=";
   };
 
   patches = [
@@ -33,7 +33,7 @@ buildNpmPackage (finalAttrs: {
   ];
 
   npmBuildScript = "build:plugin-zip";
-  npmDepsHash = "sha256-FIzDE8g/KuBlUpnU3Z4qlxN7OoJjp3MaNWbgd1DFNMA=";
+  npmDepsHash = "sha256-bD12wDpYZJ+FSwuK0eElrQixplQEekWi9bUy1/srRZE=";
 
   env.NO_CHECKS = "true"; # avoids git clean
 
