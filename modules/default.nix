8c04cc62c2a9f1649b60b897cd7c9c6e74567867
Bart Oostveen <bart@bartoostveen.nl>
2026-07-31 02:03:20 +0200

    nixos/timedout-registry: init

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 {
   # keep-sorted start
   mistserver = ./mistserver.nix;
+  timedout-registry = ./timedout-registry.nix;
   uptime-kuma-matrix = ./uptime-kuma-matrix.nix;
   # keep-sorted end
 }
--- tests/default.nix
+++ tests/default.nix
@@ -3,6 +3,7 @@ args:
 {
   # keep-sorted start
   mistserver = import ./mistserver.nix args;
+  timedout-registry = import ./timedout-registry.nix args;
   uptime-kuma-matrix = import ./uptime-kuma-matrix.nix args;
   # keep-sorted end
 }
