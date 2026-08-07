f73015853ebf9d5e8daeb1da07cc026a6e41782b
Bart Oostveen <bart@bartoostveen.nl>
2026-08-07 10:04:23 +0200

    nixos/ssacli: init

--- modules/default.nix
+++ modules/default.nix
@@ -3,6 +3,7 @@
   git-pages = ./git-pages.nix;
   maubot-exporter = ./maubot-exporter.nix;
   mistserver = ./mistserver.nix;
+  ssacli = ./ssacli.nix;
   timedout-registry = ./timedout-registry.nix;
   uptime-kuma-matrix = ./uptime-kuma-matrix.nix;
   venator = ./venator.nix;
